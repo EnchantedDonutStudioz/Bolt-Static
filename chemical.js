e9ff625d5da594ee15535554344f0d600b1d2377
EnchantedDonutStudioz <EnchantedDonutStudioz@gmail.com>
2024-10-26 15:40:06 -0400

    r

--- chemical.js
+++ chemical.js
@@ -8,11 +8,11 @@
     prefix: "/~/uv/",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: "/uv/uv.handler.js",
-    client: "/uv/uv.client.js",
-    bundle: "/uv/uv.bundle.js",
-    config: "/uv/uv.config.js",
-    sw: "/uv/uv.sw.js",
+    handler: "uv/uv.handler.js",
+    client: "uv/uv.client.js",
+    bundle: "uv/uv.bundle.js",
+    config: "uv/uv.config.js",
+    sw: "uv/uv.sw.js",
   };
 
   const currentScript = document.currentScript;
